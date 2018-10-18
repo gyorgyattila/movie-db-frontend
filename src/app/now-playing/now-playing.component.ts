a700c44263b92b0b9dd948eb7c4fb43440a86d45
Meszaros Gyorgy Attila <sirex1993@gmail.com>
2018-10-18 13:51:36 +0200

    now playing business logic

---
+++ src/app/now-playing/now-playing.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {DataStorageService} from '../shared/data-storage.service';
+import {FilmService} from '../services/film.service';
+import {FilmModel} from '../film/film.model';
+
+@Component({
+  providers: [DataStorageService],
+  selector: 'app-now-playing',
+  templateUrl: './now-playing.component.html',
+  styleUrls: ['./now-playing.component.css']
+})
+export class NowPlayingComponent implements OnInit {
+
+  films: FilmModel[] = [];
+
+  constructor(private dataStorage: DataStorageService, private filmService: FilmService) {
+  }
+
+  ngOnInit() {
+    this.filmService.professorListChanged.subscribe(
+      (films: FilmModel[]) => {
+        this.films = films;
+      }
+    );
+    this.dataStorage.getNowPlayingFilms();
+  }
+
+}
